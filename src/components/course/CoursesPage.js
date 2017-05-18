8b54ad5c09e3264f4b9401be85beaec1a49fe21b
Ricardo Pimienta <rpimienta@itexico.net>
2017-05-18 12:12:48 -0500

    another commit

---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,40 @@
+import React, {PropTypes} from 'react';
+
+class CoursePage extends React.Component{
+    constructor (props, context) {
+        super(props, context);
+        this.onTitleChange = this.onTitleChange.bind(this);
+        this.onClickSave = this.onClickSave.bind(this);
+        this.state ={
+            course: { title: null }
+        };
+    }
+
+    onTitleChange(event) {
+        const course = this.state.course;
+        course.title = event.target.value;
+        this.setState({course: course});
+    }
+
+    onClickSave () {
+        alert(`Saving ${this.state.course.title}`);
+    }
+    render() {
+        return (
+            <div>
+                <h1>Courses</h1>
+                <h2>Add Course Now</h2>
+                <input 
+                    type="text"
+                    onChange={this.onTitleChange}
+                    value={this.state.course.title}/>
+                <input 
+                    type="submit"
+                    value="Save"
+                    onClick={this.onClickSave}/>
+            </div>
+        );
+    }   
+} 
+
+export default CoursePage;
